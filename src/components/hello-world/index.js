2c52baa7681ca590df5711d282117b54b1dae7d0
Chris Rodgers <crodgers@alternativeairlines.com>
2019-05-21 12:00:50 +0100

    get rating from API

--- src/components/hello-world/index.js
+++ src/components/hello-world/index.js
@@ -1,12 +1,28 @@
 import { h, Component } from "preact";
+import request from "./request";
 import "./style.scss";
 
 export default class App extends Component {
-  render(props) {
-    return (
-      <div>
-        <h1>{props.merchant_identifier}</h1>
-      </div>
-    );
-  }
+	state = {
+		rating: null
+	};
+
+	componentDidMount = () => {
+		const { merchant_identifier } = this.props;
+		request(
+			`https://api.feefo.com/api/10/reviews/summary/all?merchant_identifier=${merchant_identifier}`
+		).then(data => {
+			const { rating } = JSON.parse(data.response).rating;
+			this.setState({rating})
+		});
+	};
+
+	render(props) {
+    const {rating} = this.state;
+		return (
+			<div>
+				<h1>Rating: {rating}</h1>
+			</div>
+		);
+	}
 }
